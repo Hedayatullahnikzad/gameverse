7e337a5b841488971ead75c737ef0fb747bf7168
Hedayatullahnikzad <nikzad.hedayatullah@gmail.com>
2026-01-06 17:04:05 +0430

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="grid grid-cols-[300px_1fr] grid-rows-[auto_1fr]">
-      <div className="bg-orange-400 col-span-2 p-2">Nav</div>
-      <div className="bg-yellow-400 p-2 hidden lg:block">Aside</div>
-      <div className="bg-blue-500 p-2 col-span-full lg:col-span-1 lg:col-start-2">
+      <div className="col-span-2">
+        <Navbar />
+      </div>
+      <div className="bg-yellow-400  hidden lg:block">Aside</div>
+      <div className="bg-blue-500 col-span-full lg:col-span-1 lg:col-start-2">
         Main
       </div>
     </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.webp";
+const Navbar = () => {
+  return (
+    <div className="flex items-center">
+      <img src={logo} alt="logo" className="w-[60px] h-[60px]" />
+      navbar
+    </div>
+  );
+};
+
+export default Navbar;
