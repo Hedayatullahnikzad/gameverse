616f209750964acc79e1c82eaca0bf1b1c763135
Hedayatullahnikzad <nikzad.hedayatullah@gmail.com>
2026-02-09 13:09:23 +0430

    Refactoring- use custom getCroppedImageUrl helper

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,23 +1,25 @@
 import type { Game } from "../hooks/useGames";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
 }
+
 const GameCard = ({ game }: Props) => {
   return (
     <>
       {/* Image */}
       <img
-        src={game.background_image}
+        src={getCroppedImageUrl(game.background_image)}
         alt={game.name}
+        loading="lazy"
         className="w-full h-48 object-cover"
       />
 
       {/* Card body */}
       <div className="p-4 space-y-2">
-        {" "}
         {/* Platforms + Critic Score */}
         <div className="flex items-center justify-between">
           <PlatformIconList
@@ -25,6 +27,7 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </div>
+
         {/* Game title */}
         <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">
           {game.name}
